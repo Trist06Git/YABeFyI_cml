d9188930319c90d6fe039183a2a8956cfa07ffaa
Trist06Git <Trist06@hotmail.com>
2019-08-21 18:05:26 +0930

    Removed old code

--- file_io.ml
+++ file_io.ml
@@ -3,17 +3,6 @@ Author  : Flesh_Bag, Trist06@hotmail.com
 licence : GPLv2
 *)
 
-let read_file_old filename = 
-  let lines = ref [] in
-  let chan = open_in filename in
-  try
-    while true do (*strangely the example had "true;", why?*)
-      lines := input_line chan :: !lines
-    done; !lines
-  with
-    (*| End_of_file -> close_in chan ;*)
-    | _ -> close_in chan ; List.rev !lines
-
 let read_file filename =
   let lines = ref [] in
   let chan = 
@@ -32,14 +21,6 @@ let read_file filename =
     with
       | End_of_file -> ( close_in chan ; List.rev !lines )
       | _           -> ( close_in chan ; List.rev !lines )
-
-let mon_lines_to_string lst =
-  let rec to_string lst str =
-    if lst = [] then
-      str
-    else
-      to_string (List.tl lst) (str ^ (List.hd lst))
-  in to_string lst ""
 
 let lines_to_string lst =
   String.concat "" lst
