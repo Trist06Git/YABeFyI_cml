c8efada04548458a2fc49383cd3c267eec26e8ef
Trist06Git <Trist06@hotmail.com>
2019-08-27 14:31:38 +0930

    Changed main loop to accommodate the new command type
    Replaced the loop function that would check chars directly from a string; with a traverse function, that traverses over a tree of commands.

--- main.ml
+++ main.ml
@@ -3,6 +3,8 @@ Author  : Flesh_Bag, Trist06@hotmail.com
 licence : GPLv2
 *)
 
+open Types
+
 let create_mem n =
   if n < 0 then [] else
   let rec create_mem n lst =
@@ -10,55 +12,45 @@ let create_mem n =
       | 0 -> lst
       | _ -> create_mem (n-1) (0 :: lst)
   in create_mem n []
-in
 
-let rec loop str brs n left right =
-  if n = String.length str then
-    Types.Fin
-  else begin
-    let c = (String.get str n) in
-    match c with
-      | '>' -> (match right with
-                  | []     -> loop str brs (n+1) left right
-                  | h :: t -> loop str brs (n+1) (h :: left) t
-               )
-      | '<' -> (match left with
-                  | _ :: [] -> loop str brs (n+1) left right
-                  | h :: t  -> loop str brs (n+1) t (h :: right)
-                  | []      -> Types.Non
-               )
-      | '+' -> (loop str brs (n+1) (((List.hd left) + 1) :: List.tl left) right)
-      | '-' -> (loop str brs (n+1) (((List.hd left) - 1) :: List.tl left) right)
-      | '.' -> (print_char (Char.chr (List.hd left)) ; loop str brs (n+1) left right)
-      | ',' -> (match left with
-                  | []     -> Types.Fin
-                  | _ :: t -> (loop str brs (n+1) ((Char.code (input_char stdin)) :: t) right)
-               )
-      | '[' -> (if List.hd left = 0 then begin
-                 match Br_check.get_right n brs with
-                   | Some x -> (loop str brs (x + 1) left right)
-                   | _      -> Types.Fin
-               end
-               else
-                 loop str brs (n+1) left right)
-      | ']' -> (match Br_check.get_left n brs with
-                  | Some x -> (loop str brs x left right)
-                  | _      -> Types.Fin
-               )
-      |  _  -> loop str brs (n+1) left right
-  end
-in
+let rec traverse cmds left right debug =
+  if debug then (match cmds with(*for debug*)
+    | [] -> ()
+    | h :: _ -> Printf.printf "cmd : %s\n" (cmd_to_string h)
+  ) ;
+  match cmds with
+    | []          -> Types.Fin
+    | Right  :: t -> (match right with
+                        | []         -> traverse t left right debug
+                        | r_h :: r_t -> traverse t (r_h :: left) r_t debug
+                     )
+    | Left   :: t -> (match left with
+                        |  _  :: []   -> traverse t left right debug
+                        | l_h :: l_t  -> traverse t l_t (l_h :: right) debug
+                        | []          -> Types.Non
+                     )
+    | Inc    :: t -> let value = if (List.hd left) = 255 then 0 else ((List.hd left) + 1) in
+                     (traverse t (value :: List.tl left) right) debug
+    | Dec    :: t -> let value = if (List.hd left) = 0 then 255 else ((List.hd left) - 1) in
+                     (traverse t (value :: List.tl left) right) debug
+    | Output :: t -> (print_char (Char.chr (List.hd left)) ; traverse t left right) debug
+    | Input  :: t -> (match left with
+                        | []       -> Types.Fin
+                        | _ :: l_t -> traverse t ((Char.code (input_char stdin)) :: l_t) right debug
+                     )
+    | Loop x :: t -> (match left with
+                        | 0 :: _ -> traverse t left right debug
+                        | _      -> traverse (x @ cmds) left right debug
+                     )
 
-let main _ =
+let _ =
   if Array.length Sys.argv < 2 then
     Printf.printf "Error, no input file\n"
   else
     let str_in = (File_io.read_file_str Sys.argv.(1)) in
-    let all_br = Br_check.proc_br str_in in
+    let debug = if Array.length Sys.argv > 2 && Sys.argv.(2) = "debug" then true else false in
 
-    match all_br with
-      | Br_check.Error -> Printf.printf "Error, bad brackets!\nnow exiting\n" ; exit 1
-      | Br_check.Ok x  -> let all_br = x
-    in
-    ignore (loop str_in all_br 0 [0] (create_mem 3000))
-in main 0
+    match Parse.parse str_in with
+      | Some prog -> (if debug then dump_cmds prog ; ignore (traverse prog [0] (create_mem 30000) debug) )
+      | Error     -> Printf.printf "Error, bad brackets!\nnow exiting\n"
+      | None      -> Printf.printf "Empty program file\nnow exiting\n"
