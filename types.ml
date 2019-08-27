0e494a69576abeeda752acb7c6bce62f3d121f9a
Trist06Git <Trist06@hotmail.com>
2019-08-27 14:23:21 +0930

    Added new tcommand type
    Now used as the main program that is to be traversed by the interpreter.
    Includes debugging functions.

--- types.ml
+++ types.ml
@@ -1,12 +1,21 @@
-type command =
+(* 
+Author  : Flesh_Bag, Trist06@hotmail.com
+licence : GPLv2
+*)
+
+type cmd =
   | Inc
   | Dec
   | Left
   | Right
-  | Print
   | Input
-  | Loop of command list
-  | Dump
+  | Output
+  | Loop of cmd list
+
+type cmd_opt =
+  | Some of cmd list
+  | None
+  | Error
 
 type result =
   | Non
@@ -16,15 +25,24 @@ type result =
   | Action of int
   | Loop of int
 
-type direction =
-  | Left
-  | Right
-
 let cmd_to_string cmd =
   match cmd with
-    | Inc   -> "Inc"
-    | Dec   -> "Dec"
-    | Left  -> "Left"
-    | Right -> "Right"
-    | Print -> "Print"
-    | _     -> ""
+    | Inc    -> "Inc"
+    | Dec    -> "Dec"
+    | Left   -> "Left"
+    | Right  -> "Right"
+    | Output -> "Output"
+    | Input  -> "Input"
+    | Loop _ -> "[Loop"
+
+let dump_cmds cmds =
+  let rec dump_cmds cmds =
+    match cmds with
+      | []     ->  Printf.printf "end] "
+      | h :: t -> (Printf.printf "%s; " (cmd_to_string h) ;
+                  match h with
+                    | Loop x -> (dump_cmds x ; dump_cmds t)
+                    | _      -> dump_cmds t
+                  )
+  in Printf.printf "program tree :\n" ; dump_cmds cmds ; Printf.printf "\n\n"
+  
